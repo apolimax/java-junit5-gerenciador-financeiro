729ed8d4e204065fbad9122d8a2c7e2f8e9631bf
Will Apolinário <willieapolinario@gmail.com>
2023-08-08 16:22:42 -0300

    ContaTest deveRejeitarContaInvalida

--- src/main/java/br/rn/will/seuBarriga/domain/Conta.java
+++ src/main/java/br/rn/will/seuBarriga/domain/Conta.java
@@ -1,11 +1,15 @@
 package br.rn.will.seuBarriga.domain;
 
+import br.rn.will.seuBarriga.domain.exceptions.ValidationException;
+
 public class Conta {
 	private Long id;
 	private String nome;
 	private Usuario usuario;
 	
 	public Conta(Long id, String nome, Usuario usuario) {
+		if (nome == null) throw new ValidationException("Nome é obrigatório");
+		if (usuario == null) throw new ValidationException("Usuário é obrigatório");
 		this.id = id;
 		this.nome = nome;
 		this.usuario = usuario;
--- src/test/java/br/rn/will/seuBarriga/domain/ContaTest.java
+++ src/test/java/br/rn/will/seuBarriga/domain/ContaTest.java
@@ -1,13 +1,22 @@
 package br.rn.will.seuBarriga.domain;
 
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import br.rn.will.seuBarriga.domain.builders.ContaBuilder;
 import br.rn.will.seuBarriga.domain.builders.UsuarioBuilder;
+import br.rn.will.seuBarriga.domain.exceptions.ValidationException;
 
+@DisplayName("Domínio: Conta")
 public class ContaTest {
 	@Test
+	@DisplayName("Deve criar uma conta válida")
 	public void deveCriarContaValida() {
 		Conta conta = ContaBuilder.novaConta().agora();
 		Assertions.assertAll("Conta", 
@@ -15,5 +24,22 @@ public class ContaTest {
 				() -> Assertions.assertEquals("Conta Valida", conta.getNome()),
 				() -> Assertions.assertEquals(UsuarioBuilder.novoUsuario().agora(), conta.getUsuario())
 		);
+	}
+	
+	@ParameterizedTest
+	@MethodSource(value = "dataProvider")
+	public void deveRejeitarContaInvalida(Long id, String nome, Usuario usuario, String mensagem) {
+		String errorMessage = Assertions.assertThrows(ValidationException.class, () -> {
+			ContaBuilder.novaConta().comId(id).comNome(nome).comUsuario(usuario).agora();
+		}).getMessage();
+		
+		Assertions.assertEquals(mensagem, errorMessage);
+	}
+	
+	private static Stream<Arguments> dataProvider() {
+		return Stream.of(
+					Arguments.of(1L, null, UsuarioBuilder.novoUsuario().agora(), "Nome é obrigatório"),
+					Arguments.of(1L, "Conta Valida", null, "Usuário é obrigatório")
+				);
 	}
 }
