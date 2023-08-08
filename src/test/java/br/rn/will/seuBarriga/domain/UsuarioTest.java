ce4cfebbdde2b31ffe2d02e73c52d9e80b6ba437
Will Apolinário <willieapolinario@gmail.com>
2023-08-08 10:54:28 -0300

    User entity get instead of set, User test assertAll

--- src/test/java/br/rn/will/seuBarriga/domain/UsuarioTest.java
+++ src/test/java/br/rn/will/seuBarriga/domain/UsuarioTest.java
@@ -2,15 +2,24 @@ package br.rn.will.seuBarriga.domain;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("Domínio: Usuário")
 public class UsuarioTest {
 	
 	@Test
+	@DisplayName("Deve criar um usuário válido")
 	public void deveCriarUsuarioValido() {
 		Usuario usuario = new Usuario(1L, "João", "j@gmail.com", "123");
 		
-		assertEquals(1L, usuario.getId());
+		Assertions.assertAll("Usuario", 
+				() -> assertEquals(1L, usuario.getId()),
+				() -> assertEquals("João", usuario.getNome()),
+				() -> assertEquals("j@gmail.com", usuario.getEmail()),
+				() -> assertEquals("123", usuario.getSenha())
+				
+		);
 	}
-
 }
